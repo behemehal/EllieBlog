8fc130c6ed300762638cc7f0603e79d3d0f7c966
Ahmetcan Aksu <ahmetcanco@gmail.com>
2021-12-02 02:36:34 +0300

    Create automated discussion maker

--- updateBlog.js
+++ updateBlog.js
@@ -2,6 +2,11 @@ var blog_per_page = 10;
 var fs = require('fs');
 const moment = require('moment');
 var fileDir = fs.readdirSync('./blogs/');
+const {
+    Octokit,
+    App
+} = require("octokit");
+
 
 //Parse key and value seperated by ':'
 function parseKeyValue(str) {
@@ -40,35 +45,68 @@ function whichLineEnding(source) {
 }
 
 var files = [];
+var blogJSON = require("./blog.json");
 //Loop over files in fileDir
-for (var i = 0; i < fileDir.length; i++) {
-    var file = fs.readFileSync("./blogs/" + fileDir[i], 'utf8');
-    var lineEnding = whichLineEnding(file);
-    var lines = file.split(lineEnding);
-    if (lines[0] != '---' || lines[5] != '---') {
-        console.error("File " + fileDir[i] + " is not a valid blog file [config-error]");
-    } else {
-        var conf = lines.slice(1, 5);
-        var md = lines.slice(6);
-        var parsedConf = convertToObject(conf.map(x => parseKeyValue(x)));
-        var correct = checkObject(parsedConf);
-        if (correct) {
-            if (moment(parsedConf.date, 'DD.MM.YYYY-HH:mm').isValid()) {
-                files.push({
-                    publisher: parsedConf.publisher,
-                    title: parsedConf.title,
-                    description: parsedConf.description,
-                    date: moment(parsedConf.date, 'DD.MM.YYYY-HH:mm').format()
-                });
-            } else {
-                console.error("File " + fileDir[i] + " has invalid date");
-            }
-        } else {
-            console.error("Error: " + fileDir[i] + " is not a valid blog file  [config-error-wrong-keys]");
-            process.exit(1);
-        }
-    }
 
-}
-files.sort((a, b) => new moment(a.date).format() - new moment(b.date).format());
-fs.writeFileSync("./blog.json", JSON.stringify(files, null, 2));
+const octokit = new Octokit({
+    auth: process.env.API_KEY
+});
+octokit.rest.users.getAuthenticated().then(async () => {
+    for (var i = 0; i < fileDir.length; i++) {
+        var file = fs.readFileSync("./blogs/" + fileDir[i], 'utf8');
+        var lineEnding = whichLineEnding(file);
+        var lines = file.split(lineEnding);
+        if (lines[0] != '---' || lines[5] != '---') {
+            console.error("File " + fileDir[i] + " is not a valid blog file [config-error]");
+        } else {
+            var conf = lines.slice(1, 5);
+            var md = lines.slice(6);
+            var parsedConf = convertToObject(conf.map(x => parseKeyValue(x)));
+            var correct = checkObject(parsedConf);
+            if (correct) {
+                if (moment(parsedConf.date, 'DD.MM.YYYY-HH:mm').isValid()) {
+                    var disq_id;
+                    if (blogJSON.findIndex(x => x.id == moment(parsedConf.date, 'DD.MM.YYYY-HH:mm').format("x")) != -1) {
+                        disq_id = blogJSON.find(x => x.id == moment(parsedConf.date, 'DD.MM.YYYY-HH:mm')).disq_id;
+                        files.push({
+                            id: moment(parsedConf.date, 'DD.MM.YYYY-HH:mm').format("x"),
+                            publisher: parsedConf.publisher,
+                            title: parsedConf.title,
+                            description: parsedConf.description,
+                            date: moment(parsedConf.date, 'DD.MM.YYYY-HH:mm').format(),
+                            disq_id
+                        });
+                    } else {
+
+                        disq_id = await octokit.graphql(
+                            `mutation {
+                                    createDiscussion(input: {repositoryId: "R_kgDOGdv8Ow", categoryId: "DIC_kwDOGdv8O84CAHDn", body: "The body", title: "The title"}) {
+                                      discussion {
+                                        id
+                                      }
+                                    }
+                                  }`
+                        );
+                        files.push({
+                            id: moment(parsedConf.date, 'DD.MM.YYYY-HH:mm').format("x"),
+                            publisher: parsedConf.publisher,
+                            title: parsedConf.title,
+                            description: parsedConf.description,
+                            date: moment(parsedConf.date, 'DD.MM.YYYY-HH:mm').format(),
+                            disq_id: disq_id.createDiscussion.discussion.id
+                        });
+                    }
+
+                } else {
+                    console.error("File " + fileDir[i] + " has invalid date");
+                }
+            } else {
+                console.error("Error: " + fileDir[i] + " is not a valid blog file  [config-error-wrong-keys]");
+                process.exit(1);
+            }
+        }
+
+    }
+    files.sort((a, b) => new moment(a.date).format() - new moment(b.date).format());
+    fs.writeFileSync("./blog.json", JSON.stringify(files, null, 2));
+})
